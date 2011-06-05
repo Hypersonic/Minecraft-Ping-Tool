ac71dbe45b06bb8b36808c06f9fd66966b226523
jayhf <jayhf614@aol.com>
2011-06-05 12:59:28 -0400

    I added some stuff

--- src/aor/PingTool/PingToolPlayerListener.java
+++ src/aor/PingTool/PingToolPlayerListener.java
@@ -10,34 +10,36 @@ import org.bukkit.event.player.PlayerListener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 
-public class PingToolPlayerListener extends PlayerListener {
-	
+public class PingToolPlayerListener extends PlayerListener {	
 	public static PingTool plugin;
-	
+	public ArrayList<Block> replacedBlocks=new ArrayList<Block>;
+	public ArrayList<Material> replacedBlocksMaterial=new ArrayList<Material>;
 	public PingToolPlayerListener(PingTool instance) {
 		plugin = instance;
 	}
-
+	public void initializeArrayList(){
+		replacedBlocks.trimtosize();
+	}
+	public void replaceBlock(){
+		//put the block back in world
+		replacedBlocks.get(0).setType(replacedBlocksMaterial.get(0));
+		replacedBlocks.remove(0);
+		replacedBlocks.trimtosize();
+		replacedBlocksMaterial.remove(0);
+		replacedBlocksMaterial.trimtosize();
+	}
 	public void onPlayerInteract(PlayerInteractEvent event){
-		
 		Player player = event.getPlayer();
-
-		
 		// Left clicking air or a block event:
-		if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK && player.getItemInHand().getType() == Material.WOOL) // If they left clicked with wool.
+		if ((event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK) && player.getItemInHand().getType() == Material.WOOL) // If they left clicked with wool.
 		{
-			Block targetBlock = player.getTargetBlock(null, 100); // Select the target block.
+			Block targetBlock = player.getTargetBlock(null, 1000); // Select the target block.
 			if (targetBlock.getType() != Material.AIR) // No pinging midair!
 			{
+				replacedBlocks.add(targetBlock);//store the block
+				replacedBlocksMaterial.add(targetBlock.getMaterial);
 				targetBlock.setType(Material.WOOL); // Turn it to wool!
-				
 			}
-
-
-			
 		}
-
-	
 	}
-
 }
