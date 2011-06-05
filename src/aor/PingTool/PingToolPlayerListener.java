a7c3f32377552da719f38c00f8755f5c6d934e1c
Josh Hofing <joshhofing@gmail.com>
2011-06-05 11:38:25 -0400

    Make it wool

--- src/aor/PingTool/PingToolPlayerListener.java
+++ src/aor/PingTool/PingToolPlayerListener.java
@@ -1,7 +1,11 @@
 package aor.PingTool;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerListener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
@@ -14,9 +18,24 @@ public class PingToolPlayerListener extends PlayerListener {
 		plugin = instance;
 	}
 
-	public void onPlayerInteract(PlayerMoveEvent event){
+	public void onPlayerInteract(PlayerInteractEvent event){
 		
 		Player player = event.getPlayer();
+
+		
+		// Left clicking air or a block event:
+		if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK && player.getItemInHand().getType() == Material.WOOL) // If they left clicked with wool.
+		{
+			Block targetBlock = player.getTargetBlock(null, 100); // Select the target block.
+			if (targetBlock.getType() != Material.AIR) // No pinging midair!
+			{
+				targetBlock.setType(Material.WOOL); // Turn it to wool!
+				
+			}
+
+
+			
+		}
 
 	
 	}
