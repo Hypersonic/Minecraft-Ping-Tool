e3bc4a85262582417357ca4da06292f8f353fa39
1000000000 <lthyde@gmail.com>
2012-09-17 10:33:29 -0400

    Revert "Merge branch 'master' of github.com:Hypersonic/Minecraft-Ping-Tool"
    This reverts commit bc120c644d455516774cb745d2ee63e125682741, reversing
    changes made to 54c7152cafd6ec1eebf594fc04af4f7eb2ba6c2f.

--- src/aor/PingTool/PingToolPlayerListener.java
+++ src/aor/PingTool/PingToolPlayerListener.java
@@ -11,83 +11,54 @@ import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.material.*;
 import java.util.*;
 import org.bukkit.scheduler.*;
-import org.bukkit.DyeColor;
 
-public class PingToolPlayerListener extends PlayerListener {    
-    public static PingTool plugin;
-    public static List<Block> replacedBlocks=new ArrayList<Block>();
-    public static List<Material> replacedBlocksMaterial=new ArrayList<Material>();
-    public static final MaterialData redWool=new MaterialData(Material.WOOL,(byte)14);
-    public byte redByte=(byte)14;
-//  public Material redwool=
-    public PingToolPlayerListener(PingTool instance) {
-        plugin = instance;
-    }
-    public static void replaceBlock(){
-        //put the block back in world
-        Block block=replacedBlocks.get(0);
-        Block tempBlock=replacedBlocks.get(0);
-        Material tempMaterial=replacedBlocksMaterial.get(0);
-        System.out.println("Stored the blocks");
-        replacedBlocks.remove(0);
-        replacedBlocksMaterial.remove(0);
-        if(!replacedBlocks.contains(block)){
-            tempBlock.setType(tempMaterial);
-            System.out.println("changed it back");
-        }
-        System.out.println("Finished");
-        
-    }
-    public void onPlayerInteract(PlayerInteractEvent event){
-        Player player = event.getPlayer();
-        // Left clicking air or a block event:
-        if ((event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK) && player.getItemInHand().getType() == Material.WOOL) // If they left clicked with wool.
-        {
-            Block targetBlock = player.getTargetBlock(null, 1000); // Select the target block.
-            if (targetBlock.getType() != Material.AIR) // No pinging midair!
-            {
-                if(replacedBlocks.size()>0){
-                    if(!replacedBlocks.contains(targetBlock)){
-                        replacedBlocks.add(targetBlock);//store the block
-                        replacedBlocksMaterial.add(targetBlock.getType());
-                        targetBlock.setType(redWool.getItemType()); // Turn it to wool!
-                        try{
-                            Thread.sleep(2000);
-                        }
-                        catch(InterruptedException ae){
-                            System.out.println(ae);
-                        }
-                        replaceBlock();
-                    }
-                    else{
-                        replacedBlocks.add(replacedBlocks.get(replacedBlocks.indexOf(targetBlock)));//store the block
-                        replacedBlocksMaterial.add(replacedBlocks.get(replacedBlocks.indexOf(targetBlock)).getType());
-                        targetBlock.setType(redWool.getItemType()); // Turn it to wool!
-                        try{
-                            Thread.sleep(2000);
-                        }
-                        catch(InterruptedException ae){
-                            System.out.println(ae);
-                        }
-                        replaceBlock();
-                    }
-                }
-                else{
-                    System.out.println("First Use Run");
-                    replacedBlocks.add(targetBlock);//store the block
-                    replacedBlocksMaterial.add(targetBlock.getType());
-                    targetBlock.setType(redWool.getItemType());
-                    System.out.println("Set to Red");
-                    try{
-                        Thread.sleep(2000);
-                    }
-                    catch(InterruptedException ae){
-                        System.out.println(ae);
-                    }
-                    System.out.println("Slept");
-                    replaceBlock();
-                }
-            }
-        }
-    }
+public class PingToolPlayerListener extends PlayerListener {	
+	public static PingTool plugin;
+	public static List<Block> replacedBlocks=new ArrayList<Block>();
+	public static List<Material> replacedBlocksMaterial=new ArrayList<Material>();
+//	public Material redwool=
+	public PingToolPlayerListener(PingTool instance) {
+		plugin = instance;
+	}
+	public static void replaceBlock(){
+		//put the block back in world
+		Block block=replacedBlocks.get(0);
+		replacedBlocks.remove(0);
+		replacedBlocksMaterial.remove(0);
+		if(!replacedBlocks.contains(block)){
+			replacedBlocks.get(0).setType(replacedBlocksMaterial.get(0));
+		}
+	}
+	public void onPlayerInteract(PlayerInteractEvent event){
+		Player player = event.getPlayer();
+		// Left clicking air or a block event:
+		if ((event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK) && player.getItemInHand().getType() == Material.WOOL) // If they left clicked with wool.
+		{
+			Block targetBlock = player.getTargetBlock(null, 1000); // Select the target block.
+			if (targetBlock.getType() != Material.AIR) // No pinging midair!
+			{
+				if(!replacedBlocks.contains(targetBlock)){
+					replacedBlocks.add(targetBlock);//store the block
+					replacedBlocksMaterial.add(targetBlock.getType());
+					targetBlock.setType(Wool.setData((byte)14)); // Turn it to wool!
+					plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+						public void run() {
+							replaceBlock();
+						}
+					}, 20L);
+				}
+				else{
+					replacedBlocks.add(replacedBlocks.get(replacedBlocks.indexOf(targetBlock)));//store the block
+					replacedBlocksMaterial.add(replacedBlocks.get(replacedBlocks.indexOf(targetBlock)).getType());
+					targetBlock.setType(Wool.setData((byte)14)); // Turn it to wool!
+					
+					plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+						public void run() {
+							replaceBlock();
+						}
+					}, 20L);
+				}
+			}
+		}
+	}
 }
